6efdc11a3141fc982ffb8f8602d7236f85bc7534
Phalanxia <ryan@ryansweigert.com>
2012-10-17 23:12:37 -0500

    Favicon, fullscreen toggle support

--- js/core.js
+++ js/core.js
@@ -26,7 +26,7 @@ function notification(i,t,a){
 	$("#songname").html(t);
 	$("#songartist").html(a);
 	$("#notification").fadeIn(200);
-	setTimeout(function() {
+	setTimeout(function(){
 		$("#notification").fadeOut(1000);
 	}, 3000);
 }
@@ -36,3 +36,7 @@ $("#nButtonSettings").click(function(){nav(1);});
 $("#dialogButton").click(function(){$("#dialogP").fadeOut();});
 $("#mD").click(function(){dialog("Error","Some random stuff happened. You should probably look into it.");});
 $("#nE").click(function(){notification("images/songexample.png","Song Name","Artist Name");});
+
+$("#nButtonFullscreen").click(function(){
+	$(document).toggleFullScreen();
+});
